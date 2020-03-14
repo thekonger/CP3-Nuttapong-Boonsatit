407eea7c3e6f6c1ec94435673a74b8504cfa4e53
thekonger <50547955+thekonger@users.noreply.github.com>
2020-03-14 19:15:26 +0700

    Add files via upload

---
+++ assignments/Exercise4_Nuttapong_B.py
@@ -0,0 +1,9 @@
+foundEng=80.5
+generalBusi=90
+introComSys=99.2
+comProgram=94
+print("---------------Your Score-----------------")
+print("Foundation English : ",foundEng)
+print("General Business : ",generalBusi)
+print("Introduction to Computer System : ",introComSys)
+print("Computer Programming : ",comProgram)
