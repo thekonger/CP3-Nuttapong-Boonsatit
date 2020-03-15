c294760656c40056513fbdf4430bcbe194dae7ab
thekonger <thekonger@users.noreply.github.com>
2020-03-15 17:52:47 +0700

    Create Lecture50_Nuttapong_B.py

---
+++ Lecture50_Nuttapong_B.py
@@ -0,0 +1,12 @@
+def addNumber(num1,num2):
+    print(num1+num2)
+def delNumber(num1,num2):
+    print(num1-num2)
+def mulNumber(num1, num2):
+    print(num1*num2)
+def divNumber(num1, num2):
+    print(num1/num2)
+addNumber(23,30)
+delNumber(40,33)
+mulNumber(23,11)
+divNumber(40,10)
